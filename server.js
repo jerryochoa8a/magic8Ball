fea4bcd7623c6e44ecd928f350d22287e519d1f5
Jerry A Ochoa <jerryochoa8a@aol.com>
2020-11-01 19:07:31 -0800

    initial commit

--- server.js
+++ server.js
@@ -9,31 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
 ////////////////   FOOD     /////////////////
-app.get('/food', function (req, res, next) {
-    axios.get("https://api.yelp.com/v3/businesses/search?term=food&location=91350", //https://api.yelp.com/v3/businesses/search?term=food&location=91350
+app.get('/:term/zip/:zip', function (req, res, next) {
+    axios.get("https://api.yelp.com/v3/businesses/search?term=" + req.params.term + "&location=" + req.params.zip, //https://api.yelp.com/v3/businesses/search?term=food&location=91350
             {headers: {Authorization: 'Bearer hPjOFcIIhHBc9gmUo9UIC-ySfb20hos1d0swBH2ri1HUEnto-C2mjbgcIYOLzoy-R9gi7ILtVmKwoz6MXbgt0MVYcaYfsv_rCMVPlV4jd-7AGsT5zDfdt_eO3F3PXnYx' } })
                     .then(response => {  //the auth key will change every 7 days!!!!
                         console.log(response.data);
-                        res.json({returned_data: response.data});
-            })
-            .catch(err => console.log(err));
-})
-///////////////    BARS    ////////////////
-app.get('/bar', function (req, res, next) {
-    axios.get("https://api.yelp.com/v3/businesses/search?term=bar&location=91350", //https://api.yelp.com/v3/businesses/search?term=food&location=91350
-            {headers: {Authorization: 'Bearer hPjOFcIIhHBc9gmUo9UIC-ySfb20hos1d0swBH2ri1HUEnto-C2mjbgcIYOLzoy-R9gi7ILtVmKwoz6MXbgt0MVYcaYfsv_rCMVPlV4jd-7AGsT5zDfdt_eO3F3PXnYx' } })
-                    .then(response => {  //the auth key will change every 7 days!!!!
-                        console.log(response.data);
-                        res.json({returned_data: response.data});
-            })
-            .catch(err => console.log(err));
-})
-///////////////   BOBA    ////////////////
-app.get('/boba', function (req, res, next) {
-    axios.get("https://api.yelp.com/v3/businesses/search?term=boba&location=91350", //https://api.yelp.com/v3/businesses/search?term=food&location=91350
-            {headers: {Authorization: 'Bearer hPjOFcIIhHBc9gmUo9UIC-ySfb20hos1d0swBH2ri1HUEnto-C2mjbgcIYOLzoy-R9gi7ILtVmKwoz6MXbgt0MVYcaYfsv_rCMVPlV4jd-7AGsT5zDfdt_eO3F3PXnYx' } })
-                    .then(response => {  //the auth key will change every 7 days!!!!
-                        console.log(response.data);
+                        console.log('-----' + req.params.zip + '-------') //req.params.zip gets the zip var from "app.get"
                         res.json({returned_data: response.data});
             })
             .catch(err => console.log(err));
